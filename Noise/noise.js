ac87f78ec6e6c4a2199ad46c5ba7f77e42784fef
Ilya Medyanikov <ilya.medyanikov@gmail.com>
2020-08-30 01:16:06 +0300

    Remove unused code

--- Noise/noise.js
+++ Noise/noise.js
@@ -16,8 +16,7 @@ function draw() {
   background(0);
   for (let x = 0; x < width; x += 20)
     for (let y = 0; y < height; y += 20) {
-        let n = noise((x + frameCount * 3) / 100, (y + frameCount * 0) / 100);
-        let hue = n * 100;
+        let n = noise((x + frameCount * 3) / 100, y / 100);
         noStroke();
         fill(80 * n, 80 * n, 80);
         rect(x + n * 15, y + n * 15, 3 + n * 10, 3 + n * 10, n * 4, n * 4, n * 4, n * 4);
